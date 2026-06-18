98c7624d259867f1642786311cd3030cbd619359
Malvika52 <56530699+Malvika52@users.noreply.github.com>
2026-06-18 14:16:00 +0530

    Add files via upload

--- src/main/java/com/moviz/app/example/retrofit/ApiService.kt
+++ src/main/java/com/moviz/app/example/retrofit/ApiService.kt
@@ -1,1 +1,13 @@
+package com.moviz.app.example.retrofit
 
+
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("/")
+    suspend fun getMovieList() : List<Movie>
+
+}
